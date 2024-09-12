30cd253962666c174ebda46b9196a1975f2bcb9f
rostand kinfack <kinfackrostand@gmail.com>
2024-09-12 12:58:50 +0200

    assert

---
+++ src/test/java/com/rostand/store/tests/echecTest.java
@@ -0,0 +1,26 @@
+package com.rostand.store.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class echecTest {
+/**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test_add_to_chart()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void test_login_feature()
+    {
+        assertTrue( false );
+    }
+    @Test
+    public void test_sign_feature()
+    {
+        assertTrue( true );
+    }
+}
